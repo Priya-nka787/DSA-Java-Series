fcaa44bfe607c7953a6cd2e8ed7d66d2a191e7db
Priyanka Choudhary <pika.jpr2005@gmail.com>
2026-05-18 23:33:01 +0530

    Added datatype program

--- src/Datatypes.java
+++ src/Datatypes.java
@@ -1,5 +1,26 @@
 public class Datatypes {
     static void main() {
-
+//     Numeric Datatype- int, byte, short, long
+        byte num1 = 127;
+        System.out.println(num1);
+        short num2 = 32767;
+        System.out.println(num2);
+        int num3 = 234576924;
+        System.out.println(num3);
+        long num4 = 2138679020;
+        System.out.println(num4);
+//        floating datatype - float, double
+        float num5 = 3.142436f;
+        System.out.println(num5);
+        double num6 = 1.2369740502074;
+        System.out.println(num6);
+//     Other - char, boolean
+        boolean eligibleToVote = true;
+        System.out.println(eligibleToVote);
+        char firstCharacter = 'a';
+        System.out.println(firstCharacter);
+//        ASCII value concept
+        System.out.println(firstCharacter + 2);
+        System.out.println((char)(firstCharacter+2));
     }
 }
