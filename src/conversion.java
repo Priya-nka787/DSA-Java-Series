7571b44f469de26668fc7efabe5df139cafbe7a1
Priyanka Choudhary <pika.jpr2005@gmail.com>
2026-05-19 22:49:09 +0530

    Explicit conversion

---
+++ src/conversion.java
@@ -0,0 +1,8 @@
+public class conversion {
+    static void main() {
+//        explicit conversion
+        long value1 = 1234567890;
+        int value2= (int)value1;
+        System.out.println(value2);
+    }
+}
